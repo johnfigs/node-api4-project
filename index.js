26829e13e0a966a00b54df4268fcf997e4704297
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-09-30 18:42:32 -0700

    added res.send h1 to '/'

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const path = require('path')
 
 const port = process.env.PORT || 3000
 
+server.get('/', (req, res) => {
+    res.send(`<h1>HELLO THERE</h1>`)
+})
+
 server.listen(port, () => {
     console.log(`listening on ${port}`)
 })
